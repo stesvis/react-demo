c7b9c2950d85ce18088dd444ec1f2e6c0a33fec5
Cristian Merli <stesvis@gmail.com>
2022-05-25 09:11:50 -0600

    Applied styles

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -2,6 +2,8 @@ import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import React, { useState } from "react";
 
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { Box } from "@mui/system";
+import { Form } from "react-bootstrap";
 import { TextField } from "@mui/material";
 
 const AppDatePicker = ({ name, placeholder, value, ...otherProps }) => {
@@ -10,32 +12,40 @@ const AppDatePicker = ({ name, placeholder, value, ...otherProps }) => {
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <DatePicker
+        Prop
+        OpenPickerButtonProps={{
+          sx: { color: "#eeeeee" },
+        }}
         InputProps={{
           disableUnderline: true,
+          sx: {
+            "& .MuiInput-input": { color: "#eeeeee" },
+          },
         }}
         onChange={setDate}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            className="form-control"
-            variant="standard"
-            inputProps={{ className: "form-control" }}
-          />
-        )}
-        // renderInput={({ inputRef, inputProps, InputProps }) => (
-        //   <Box
-        //     sx={{
-        //       display: "flex",
-        //       alignItems: "center",
-        //       backgroundColor: "#282d36",
-        //       borderRadius: ".25rem",
-        //       paddingRight: "8px",
-        //     }}>
-        //     <Form.Control ref={inputRef} {...inputProps}></Form.Control>
-        //     {InputProps?.endAdornment}
-        //   </Box>
+        // readOnly
+        // renderInput={(params) => (
+        //   <TextField
+        //     {...params}
+        //     className="form-control"
+        //     aria-readonly="true"
+        //     variant="standard"
+        //     // inputProps={{ className: "form-control" }}
+        //   />
         // )}
-        toolbarPlaceholder="aaaa"
+        renderInput={({ inputRef, inputProps, InputProps }) => (
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              backgroundColor: "#282d36",
+              borderRadius: ".25rem",
+              paddingRight: "8px",
+            }}>
+            <Form.Control ref={inputRef} {...inputProps}></Form.Control>
+            {InputProps?.endAdornment}
+          </Box>
+        )}
         value={date}
       />
     </LocalizationProvider>
